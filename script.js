13a6ed26478adad370375dda54283292c1662d1a
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-11-25 11:40:19 +0100

    feat: delete button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 const rows = document.getElementsByClassName("row");
 const dialog = document.querySelector("dialog");
 const dialogButton = document.getElementById("dialog-button");
@@ -39,18 +39,56 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(book);
 }
 
+function createBookStructure(bookObj, book) {
+  const trashIcon = document.createElement("img");
+  trashIcon.src = "./icons/trash-2.svg";
+  trashIcon.dataset.bookId = book.id;
+  bookObj.deleteButton.appendChild(trashIcon)
+  bookObj.bookButtons.appendChild(bookObj.deleteButton);
+  bookObj.bookButtons.appendChild(bookObj.readButton);
+  bookObj.bookElement.appendChild(bookObj.text);
+  bookObj.bookElement.appendChild(bookObj.bookButtons);
+
+  bookObj.bookElement.className = "book";
+  bookObj.bookButtons.className = "book-buttons";
+  bookObj.deleteButton.className = "delete";
+  bookObj.deleteButton.dataset.bookId = book.id;
+  bookObj.deleteButton.backgroundImage = "./icons/trash-2.svg";
+  bookObj.readButton.className = "has-read";
+  bookObj.readButton.dataset.bookId = book.id;
+  bookObj.readButton.textContent = "read";
+  bookObj.text.textContent = `Title: ${book.title} Author: ${book.author}`;
+}
+
+function addEventTobuttons (bookObj, book) {
+  bookObj.deleteButton.addEventListener("click", (event) => {
+    myLibrary = myLibrary.filter(book => book.id !== event.target.dataset.bookId);
+    displayBooks(myLibrary);
+  });
+
+  
+}
+
 function displayBooks(myLibrary) {
   for (let row of rows) {
     row.textContent = "";
   }
   myLibrary.forEach(book => {
-    const bookElement = document.createElement("div");
-    bookElement.className = "book";
-    bookElement.textContent = `${book.title} ${book.author}`;
+    const bookObj = {
+      bookElement: document.createElement("div"),
+      bookButtons: document.createElement("div"),
+      deleteButton: document.createElement("button"),
+      readButton: document.createElement("button"),
+      text: document.createElement("p"),
+    }
+
+    createBookStructure(bookObj, book);
+    addEventTobuttons(bookObj, book);
+
     for (let row of rows) {
       row.style.overflow = "Hidden";
       if (row.clientWidth >= row.scrollWidth) {
-        row.appendChild(bookElement);
+        row.appendChild(bookObj.bookElement);
         break;
       }
     }
